5664a380fd1aa9a658e82fc03a8d7e22d8a94add
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-28 22:17:32 +0530

    [Vivek] Completed Usecase 1 Ability to create Contacts

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookMain addressBookMain = new AddressBookMain("Vivek", "Owhal", "vivek@gmail.com", "Mumbai","Maharashtra", 8660154789, 412854);
+            addressBookMain.showContactDetails();
+        }
+    }
+}
