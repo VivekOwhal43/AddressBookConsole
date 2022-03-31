e7c9a7add78940f4857cf64555917c662165d23b
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-31 22:19:14 +0530

    [Vivek] Completed UC3-Ability-To-Edit-Contact-Using-Name

--- AddressBook/ContactDetails/AddressBookMain.cs
+++ AddressBook/ContactDetails/AddressBookMain.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace AddressBook
 {
     public class AddressBookMain
@@ -5,11 +8,12 @@ namespace AddressBook
         //NLog Iementation
         // private readonly NLogDemo nLogDemo = new NLogDemo();
         public List<AddContact> contactList = new List<AddContact>();
-        
-
+        int continueloop;
+        int ch;
         public void addContactDetails()
         {
             AddContact addContact = new AddContact();
+            Console.WriteLine("============ Enter Contact Details =========");
             Console.WriteLine("Enter First Name:");
             addContact.firstName = Console.ReadLine();
             //nLogDemo.LogDebug("SuccessS done okay");
@@ -43,6 +47,88 @@ namespace AddressBook
                 Console.WriteLine("Postal Code : " + details.postalCode);
                 contactCount++;
             }
+        }
+
+        public void editContact()
+        {
+            do
+            {
+                Console.WriteLine("Select one option from Menu \n1. Edit Contact Using Name");
+                Console.Write("Enter Name : ");
+                string searchName = Console.ReadLine();
+                foreach (var contact in contactList)
+                {
+                    if (searchName == contact.firstName)
+                    {
+                        // contactList.Remove(contact);
+                        // Console.Write($"Enter Modification Details: ");
+                        // addContactDetails();
+                        Console.Write($"Which Field You Want to Update?");
+                        Console.WriteLine($"\n1. First Name \n2. Last Name \n3. City Name \n4. State Name \n5. Contact Number \n6. Email \n7. Postal Code");
+                        int updateChoice = Convert.ToInt32(Console.ReadLine());
+                        switch (updateChoice)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter First Name:");
+                                string fname = Console.ReadLine();
+                                contact.firstName = fname;
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Enter Last Name:");
+                                string lname = Console.ReadLine();
+                                contact.lastName = lname;
+                                break;
+
+                            case 3:
+                                Console.WriteLine("Enter City Name:");
+                                string ctname = Console.ReadLine();
+                                contact.city = ctname;
+                                break;
+
+                            case 4:
+                                Console.WriteLine("Enter State Name:");
+                                string stname = Console.ReadLine();
+                                contact.state = stname;
+                                break;
+
+                            case 5:
+                                Console.WriteLine("Enter Contact Number:");
+                                double contNum = Convert.ToInt64(Console.ReadLine());
+                                contact.contactNumber = contNum;
+                                break;
+
+                            case 6:
+                                Console.WriteLine("Enter First Name:");
+                                string eml = Console.ReadLine();
+                                contact.email = eml;
+                                break;
+
+                            case 7:
+                                Console.WriteLine("Enter Postal Code:");
+                                int pCode = Convert.ToInt32(Console.ReadLine());
+                                contact.contactNumber = pCode;
+                                break;
+
+                            default:
+                                Console.WriteLine("Please enter value within specified range");
+                                break;
+                        }
+                    }
+                    else if (contactList.Count.Equals(0))
+                    {
+                        Console.WriteLine("No contacts has been added in list \n Please add contacts and try again");
+                    }
+                    else
+                    {
+                        Console.Write($"\nContact Not Found");
+                    }
+                }
+                //Console.WriteLine("1.Edit Contact Using Name");
+                Console.WriteLine("Do you want to continue? \nEnter 1 For YES and 2 For NO");
+                ch = Convert.ToInt32(Console.ReadLine());
+            } while (ch != 2);
+            Console.WriteLine("Exit from Edit Contact.");
         }
     }
 }
